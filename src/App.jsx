2407c6cc12f73bb08a9e7b5c0c822c7a89af6568
Rewati Warhade <159109059+rewati1616@users.noreply.github.com>
2026-06-25 17:29:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const users = [
     img:"https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     intro: "Highly engaged customer with strong brand loyalty and frequent purchases.",
     color:"royalblue",
-    tag: "Loyal"
+    tag: "Loyal"  
   },
   {
     img:"https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
